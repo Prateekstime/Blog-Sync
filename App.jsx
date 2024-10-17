44ea11d2905544f1899789788f9b97fffc4e2110
Prateek Tripathi <115889269+Prateekstime@users.noreply.github.com>
2024-10-18 00:55:18 +0530

    implementing design and UI

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Header from './assets/header'
+import Home from './assets/home'
+import Footer from './assets/footer'
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <Header></Header>
+    <Home></Home>
+    <h1>Vite + React</h1>
+    <Footer></Footer>
+    
+   
+    </>
+  )
+}
+
+export default App
